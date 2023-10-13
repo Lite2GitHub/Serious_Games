6356428838522b2b973027f4595f6762d1d25a9e
Lite2GitHub <ngdiyi2@gmail.com>
2023-10-13 20:25:24 +1000

    other half

---
+++ Serious_Games/Intermediate/Source/Serious_Games.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class Serious_Games : ModuleRules
+{
+	public Serious_Games(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
---
+++ Serious_Games/Intermediate/Source/Serious_Games.cpp
@@ -0,0 +1,4 @@
+#include "CoreTypes.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultModuleImpl, Serious_Games, "Serious_Games");
